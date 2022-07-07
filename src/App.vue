dd9d84bf92877cadb34e9a20ec9a110e73dfae35
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-07-07 21:14:56 +0500

    feat(App): add Main and Header components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header />
+  <Main title="они должны что-то знать" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Header,
+    Main,
+  },
+};
 </script>
 
 <style>
 #app {
+  position: relative;
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  max-width: 1440px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
