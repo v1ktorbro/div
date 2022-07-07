0577cdb218f8f4ee032e820dda5c362bc26c7d84
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-07-08 03:08:55 +0500

    refactor(SliderImageContainer): rename SliderContainer to SliderImageContainer

---
+++ src/components/SliderImageContainer.vue
@@ -0,0 +1,52 @@
+<template>
+  <article class="slider-image-container">
+    <SliderImage description="Для примера мы показали вам его лицо.  В первой серии он прячется в подвале 
+за мониторами, и пусть борода не собьёт вас с толку. Найдите героя и нажмите  на паузу — 
+ему не терпится отдать вам промокод." btnName="Искать промокод" />
+    <span class="slider-image-container__emblem">{{ emblem.toLowerCase() }}</span>
+  </article>
+</template>
+
+<script>
+import SliderImage from '../components/SliderImage.vue'
+export default {
+  name: 'SliderContainer',
+  props: {
+    emblem: String,
+  },
+  components: {
+    SliderImage,
+  },
+};
+</script>
+
+<style lang="scss">
+$cloudsCoverOnPhoto: url('../images/clouds_in_front_picture.png');
+$arrowBtnImage: url('../images/icon_middle_arrow.svg');
+.slider-image-container {
+  display: flex;
+  justify-content: center;
+  &::after {
+    content: $cloudsCoverOnPhoto;
+    position: absolute;
+    background-image: $cloudsCoverOnPhoto;
+    right: -44rem;
+    bottom: -21rem;
+    z-index: 0;
+    margin-right: auto;
+    
+  }
+}
+.slider-image-container__emblem {
+  grid-area: emblem;
+  font-family: 'Kinopoisk';
+  font-size: 14px;
+  line-height: 15px;
+  position: absolute;
+  right: 0;
+  top: 50%;
+  transform-origin: left;
+  transform: translate(50%, -100%) rotate(90deg) ;
+  color: #333333;
+}
+</style>
