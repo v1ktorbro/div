a46a853cb6c19e215f38cfa937b3da5280e6d99a
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-07-09 16:02:07 +0500

    feat(assets): add initialDb with images and description to SliderImage comp

---
+++ src/assets/initialDb.js
@@ -0,0 +1,34 @@
+export const initialDb = [
+  {
+    "imageSrc": "../images/main_image_1.png",
+    "description": "Для примера мы показали вам его лицо.  В первой серии он прячется в подвале за мониторами, и пусть борода не собьёт вас с толку. Найдите героя и нажмите  на паузу — ему не терпится отдать вам промокод."
+  },
+  {
+    "imageSrc": "../images/main_image_2.png",
+    "description": "Есть идея — есть IKEA"
+  },
+  {
+    "imageSrc": "../images/main_image_3.png",
+    "description": "Иногда лучше жевать, чем говорить"
+  },
+  {
+    "imageSrc": "../images/main_image_4.png",
+    "description": "Овип Локос! Во имя добра"
+  },
+  {
+    "imageSrc": "../images/main_image_5.png",
+    "description": "Траст» — он как я, только банк"
+  },
+  {
+    "imageSrc": "../images/main_image_6.png",
+    "description": "Просто добавь воды"
+  },
+  {
+    "imageSrc": "../images/main_image_7.png",
+    "description": "Танки грязи не боятся"
+  },
+  {
+    "imageSrc": "../src/images/main_image_8.png",
+    "description": "Где наслаждение, там я"
+  }
+];
